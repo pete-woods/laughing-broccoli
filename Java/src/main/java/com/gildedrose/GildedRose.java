c61d77491a40c1c1db6e95f4bba9f169894d1568
Pete Woods <email@pete-woods.com>
2019-05-04 21:43:56 +0100

    Extract name variable, add assertions about fullness

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -1,5 +1,7 @@
 package com.gildedrose;
 
+import java.util.Objects;
+
 class GildedRose {
     public static final String CONJURED = "Conjured Mana Cake";
 
@@ -12,10 +14,15 @@ class GildedRose {
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
             Item item = items[i];
-            if (!item.name.equals("Aged Brie")
-                    && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            Objects.requireNonNull(item);
+
+            String name = item.name;
+            Objects.requireNonNull(item.name);
+
+            if (!name.equals("Aged Brie")
+                    && !name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                 if (item.quality > 0) {
-                    if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                    if (!name.equals("Sulfuras, Hand of Ragnaros")) {
                         item.quality -= calculateRate(item);
                     }
                 }
@@ -23,7 +30,7 @@ class GildedRose {
                 if (item.quality < 50) {
                     item.quality = item.quality + 1;
 
-                    if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                         if (item.sellIn < 11) {
                             if (item.quality < 50) {
                                 item.quality = item.quality + 1;
@@ -39,15 +46,15 @@ class GildedRose {
                 }
             }
 
-            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+            if (!name.equals("Sulfuras, Hand of Ragnaros")) {
                 item.sellIn = item.sellIn - 1;
             }
 
             if (item.sellIn < 0) {
-                if (!item.name.equals("Aged Brie")) {
-                    if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                if (!name.equals("Aged Brie")) {
+                    if (!name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                         if (item.quality > 0) {
-                            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                            if (!name.equals("Sulfuras, Hand of Ragnaros")) {
                                 item.quality -= calculateRate(item);
                             }
                         }
